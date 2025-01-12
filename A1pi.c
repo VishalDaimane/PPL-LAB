9525ff82bdb893ae404b7d43bb6fcd13a5f5d41d
Vishal Prakash Daimane <142233215+VishalDaimane@users.noreply.github.com>
2025-01-12 19:20:07 +0530

    Update A1pi.c

--- A1pi.c
+++ A1pi.c
@@ -2,34 +2,23 @@
 #include <omp.h>
 
 int main() {
-    int num_steps = 1000000; // Number of steps for approximation
-    double step = 1.0 / (double)num_steps;
-    double pi = 0.0;         // Resultant value of PI
+    int n = 1000000; // Number of iterations
+    double pi = 0.0;
     int i;
 
-    // Start parallel region
-    #pragma omp parallel
-    {
-        double sum = 0.0; // Local sum for each thread
-
-        #pragma omp for
-        for (i = 0; i < num_steps; i++) {
-            double x = (i + 0.5) * step;
-            sum += 4.0 / (1.0 + x * x);
-        }
-
-        // Critical section to update the global variable
+    // Calculate PI using the Leibniz formula with critical section
+    #pragma omp parallel for
+    for (i = 0; i < n; i++) {
+        double x = ((i % 2 == 0) ? 1.0 : -1.0) / (2 * i + 1);  // Leibniz formula part
+        
         #pragma omp critical
-        {
-            pi += sum;
-        }
+        pi += x;  // Critical section to update pi
     }
 
-    // Final calculation of PI
-    pi *= step;
+    pi *= 4; // Final calculation of PI
 
-    // Print the result
-    printf("Calculated value of PI: %.15f\n", pi);
+    // Print the calculated value of PI
+    printf("Estimated value of PI: %lf\n", pi);
 
     return 0;
 }
